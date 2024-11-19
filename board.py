d9f00af3ad8ab1b215118b0539a1ffbb49badcee
Slawek Blicharz <sbandq@gmail.com>
2024-11-19 18:57:21 +0100

    letter weights, dictionary, board representation change

--- board.py
+++ board.py
@@ -2,6 +2,17 @@ class IllegalMove(Exception):
     pass
 
 
+class Tile:
+    def __init__(self, tile, char):
+        # tile is what is written on the tile, char is the underlying character
+        self.tile = tile
+        self.char = char
+
+    @staticmethod
+    def of(char):
+        return Tile(char.upper(), char.upper())
+
+
 class Board:
 
     def __init__(self):
@@ -12,30 +23,33 @@ class Board:
         board.matrix = self.matrix
         return board
 
-    def place_letter(self, letter, row: int, col: int):
-        upper = letter.upper()
+    def place_tile(self, tile, row: int, col: int):
         curr = self.matrix[row][col]
-        if curr is None or curr == upper:
-            self.matrix[row][col] = upper
+        if curr is None or curr.char == tile.char:
+            self.matrix[row][col] = tile
         else:
             raise IllegalMove
 
-    def place(self, word, row, col, horizontal):
+    def place_tiles(self, word, row, col, horizontal):
         size = len(word)
         if horizontal:
             if col + size > 15:
                 raise IllegalMove
             for i in range(size):
-                self.place_letter(word[i], row, col + i)
+                self.place_tile(word[i], row, col + i)
         else:
             if row + size > 15:
                 raise IllegalMove
             for i in range(size):
-                self.place_letter(word[i], row + i, col)
+                self.place_tile(word[i], row + i, col)
+
+    def place(self, word, row, col, horizontal):
+        tiles = [Tile.of(ch) for ch in word]
+        self.place_tiles(tiles, row, col, horizontal)
 
     def print(self):
         print(' ' + ' _ ' * 15)
         for row in self.matrix:
-            row_string = '|' + ''.join('   ' if ch is None else f' {ch} ' for ch in row) + '|'
+            row_string = '|' + ''.join('   ' if ch is None else f' {ch.tile} ' for ch in row) + '|'
             print(row_string)
         print(' ' + ' ‾ ' * 15)
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from board import Board
+from board import Tile
 from scores import Score
 
-score = Score()
-score.print()
+board = Board()
+board.place('masakra', 1, 1, True)
+board.place('ameba', 0, 1, False)
+board.place('walka', 4, 0, True)
+word_with_blank = [Tile('.', 'Ż'), Tile('A', 'A'), Tile('B', 'B'), Tile('A', 'A')]
+board.place_tiles(word_with_blank, 3, 4, False)
+board.print()
